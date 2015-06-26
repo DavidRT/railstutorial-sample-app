5a5532b6a847e323b43f6054ec726a70a944d55c
David Rodriguez <drodtach@gmail.com>
2015-06-26 00:27:57 -0300

    Complete exercises from chapter 4

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -13,6 +13,19 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
  		assert_select "a[href=?]", help_path
  		assert_select "a[href=?]", about_path
  		assert_select "a[href=?]", contact_path
+ 		assert_select "a[href=?]", signup_path
+
+ 		# for testing that title in each page is correct
+ 		get signup_path 
+ 			assert_select "title", full_title("Sign up")
+ 		get contact_path
+ 			assert_select "title", full_title("Contact")
+ 		get about_path
+ 			assert_select "title", full_title("About")
+ 		get help_path
+ 			assert_select "title", full_title("Help")
+ 		get root_path
+ 			assert_select "title", full_title("")
  	end
 
 end
